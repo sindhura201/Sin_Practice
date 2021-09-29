244c1ff6a03eb52a8ccb683407e05b8f33e19866
sindhurapatibandla07@gmail.com <sindhurapatibandla07@gmail.com>
2021-09-29 09:07:00 +0000

    Initial Commit

--- models/New_model.sql
+++ models/New_model.sql
@@ -1,6 +1,21 @@
-{{ config(
-materialized =''table''
-)}}
-select * from SINDHURA_DB.DBT_SPATIBANDLA.MY_FIRST_DBT_MODEL
+create schema sindhura_DB.sin_shop;
+
+-- create this one directly in the schema
+create table sindhura_DB.sin_shop.customers
+(
+    id integer,
+    first_name varchar,
+    last_name varchar
+);
 
 
+copy into sindhura_DB.sin_shop.customers (id, first_name, last_name)
+    from 's3://dbt-tutorial-public/jaffle_shop_customers.csv'
+        file_format = (
+            type = 'CSV'
+            field_delimiter = ','
+            skip_header = 1
+        )
+;
+
+select * from customers
